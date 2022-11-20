2848046de56775a0cf4b9b890df0cee3e009f714
BIT <vladov.ru@gmail.com>
2022-11-20 23:30:41 +0300

    Первая программа для показа таблиц

---
+++ UIToPy.bat
@@ -0,0 +1,2 @@
+C:\Users\BIT\.conda\envs\SUBD\Scripts\pyuic6.exe -x C:\Users\BIT\Desktop\education\7sem\SUBD\PythonProject\MainForm.ui -o C
+:\Users\BIT\Desktop\education\7sem\SUBD\PythonProject\MainForm.py
